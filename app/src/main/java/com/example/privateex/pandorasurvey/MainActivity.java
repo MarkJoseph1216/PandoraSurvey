92cfbb12d9ff6fd8e14438bc418442f7f047e063
unknown <work.jerson.baltazar@gmail.com>
2019-06-14 21:45:02 +0800

    creating settings ui

--- app/src/main/java/com/example/privateex/pandorasurvey/MainActivity.java
+++ app/src/main/java/com/example/privateex/pandorasurvey/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.privateex.pandorasurvey;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,9 +19,11 @@ import android.widget.TextView;
 public class  MainActivity extends AppCompatActivity {
 
     ImageView imgLucerne;
-    Button btnGetStarted;
+    Button btnGetStarted, btnSettings;
     TextView txtSurvey, txtLoading;
     ProgressBar loadingBar;
+    Dialog dialogSettings;
+    Button btnCancel, btnEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +31,17 @@ public class  MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnGetStarted = (Button) findViewById(R.id.btnGetStarted);
+        btnSettings = (Button) findViewById(R.id.btnSettings);
         imgLucerne = (ImageView) findViewById(R.id.imgLucerne);
         txtSurvey = (TextView) findViewById(R.id.txtSurvey);
         txtLoading = (TextView) findViewById(R.id.txtLoading);
         loadingBar = (ProgressBar) findViewById(R.id.loadingBar);
+
+        dialogSettings = new Dialog(MainActivity.this);
+
         imgLucerne.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoomin));
         btnGetStarted.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.downtoup));
+        btnSettings.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.downtoup));
 
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -64,5 +74,29 @@ public class  MainActivity extends AppCompatActivity {
                 }, 2500);
             }
         });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMessageSettings();
+            }
+        });
+    }
+
+    public void showPopupMessageSettings() {
+        dialogSettings.setContentView(R.layout.dialog_settings);
+        btnCancel = (Button) dialogSettings.findViewById(R.id.btnCancel);
+        btnEnter = (Button) dialogSettings.findViewById(R.id.btnEnter);
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogSettings.dismiss();
+            }
+        });
+
+        dialogSettings.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialogSettings.setCanceledOnTouchOutside(false);
+        dialogSettings.show();
     }
 }
